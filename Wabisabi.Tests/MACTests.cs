7be0720134a319a119fb4b0925680ded4543c305
lontivero <lucasontivero@gmail.com>
2020-05-06 17:53:25 -0300

    Improve MAC test

--- Wabisabi.Tests/MACTests.cs
+++ Wabisabi.Tests/MACTests.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using NBitcoin.Secp256k1;
 using Xunit;
 
 namespace Wabisabi.Tests
@@ -7,16 +5,16 @@ namespace Wabisabi.Tests
 	public class MACTests
 	{
 		[Fact]
-		public void x()
+		public void CanProduceAndVerifyMACs()
 		{
-			var w = Crypto.RandomScalar();
 			var sk = (Crypto.RandomScalar(), Crypto.RandomScalar());
 
 			var Mv = new GroupAttribute( 21_000_000);
-			var Ms = new GroupAttribute(12_3456_789);
-			var mac = MAC.Compute(w, sk, Mv + Ms);
+			var Ms = new GroupAttribute(123_456_789);
+			var mac = MAC.Compute(sk, Mv + Ms);
 
-			Assert.True(MAC.Verify(w, sk, Mv + Ms, mac));
+			Assert.True(MAC.Verify(sk, Mv + Ms, mac));
+			Assert.True(MAC.Verify(sk, Ms + Mv, mac));
 		}
 	}
 }
